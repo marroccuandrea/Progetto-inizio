9b9056596907910bca015714635e1a4524623a8a
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-20 16:00:46 +0100

    handling events (onClick)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -20,6 +24,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
