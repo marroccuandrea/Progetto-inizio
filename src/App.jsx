bfb96ed8dd1aacabdb222d938c831eb2d7c80d44
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-26 13:59:54 +0100

    Dynamic list data

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* METODO CON SPREAD OPERATOR */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
