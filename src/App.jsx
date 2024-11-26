2686127f2f2e59a6f7db2ad41b91ae45c2775870
marroccuandrea <marroccuandrea@yahoo.it>
2024-11-26 14:03:03 +0100

    [add] key

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept {...conceptItem} />
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
         </section>
